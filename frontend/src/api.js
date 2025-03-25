df707f82cdba2b9a2df719aea77911f53ddaf05c
Yauheniya <yauheniya.ai@gmail.com>
2025-03-25 23:02:36 +0100

    Add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "https://50104339-2cd2-47ce-8e78-89e2c41737b1-dev.e1-eu-north-azure.choreoapis.dev/django-react-notes/backend-gl/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
